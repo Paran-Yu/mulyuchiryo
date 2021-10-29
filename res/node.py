84d24216c271db48a89a85708e0638c6e5c3f59d
h_nuls55 <h_nuls55@naver.com>
2021-10-29 13:48:50 +0900

    [Vehicle] fix: Node class 수정

--- res/node.py
+++ res/node.py
@@ -2,9 +2,7 @@ class Node:
     def __init__(self, name_input, x_input, y_input):
         super().__init__()
 
-        self.name = name_input
-        self.x = x_input
-        self.y = y_input
+        self.NUM = name_input
+        self.X = x_input
+        self.Y = y_input
         self.isCross = False
-        self.isPort = False
-        self.isWait = False
