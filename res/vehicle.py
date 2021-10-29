0cce65f3acb00a99183f5a70a3efdb24606455a2
tyl1996 <tyl1996@gmail.com>
2021-10-29 15:34:10 +0900

    fix: added NODE_LIST

--- res/vehicle.py
+++ res/vehicle.py
@@ -36,7 +36,7 @@ class Vehicle:
         self.x = -1
         self.y = -1
         self.node = -1
-        self.next_node = -1
+        self.desti_node = -1
         self.velocity = -1
         self.angle = -1
         self.status = 0
@@ -44,41 +44,39 @@ class Vehicle:
         self.battery = -1
         self.path = []  # 각 노드(경유지)가 object일지 좌표일지 정해야할듯
         self.command_list = []
+        self.count = 0
 
     # new_node로 이동
     def move(self, new_node, path, status):
-        if self.path.length == 0:
-            self.path = path    # new_node는 path의 마지막 (path[-1])
-            self.next_node = new_node
-            self.status = status    # 충전소로 가는건지, 다음 load 장소로 가는건지-> new_node를 보고 알 수 있을까
-
+        if self.status in [00, 10, 20, 22, 80] or self.path.length == 0:    
+            self.path = path    
+            self.desti_node = new_node  # new_node는 path의 마지막 (path[-1])
+            self.status = status    # 충전소로 가서 충전하는건지, 대기하러 가는건지 status 정보 필요
         else:
-            # Core에 에러쏴주기: 이미 명령받고 이동 중
-            # 대기/충전하러 이동하는 경우에는 명령 내려도 되지 않나?
+            # Core에 에러쏴주기: 이미 명령받고 이동(21, 40, 80) 중인 경우
             pass
 
     def load(self, port_num):
         self.loaded = 1
+        # PORT[port_num].LOAD()
         pass
 
     def unload(self, port_num):
         self.loaded = 0
+        # PORT[port_num].UNLOAD()
         pass
 
     def getNode(self):
-        if self.path.length != 0:   # 아직 이동중
-            return self.path[0]     # 다음에 경유할 노드 (self.node와 동일)
-        else:                       # 이동 끝
-            return self.node        # 
+        return self.node        # 이동중일때는 경유하게 될 노드, 그렇지 않은 경우 현재 위치의 노드 반환
 
     def getPos(self):
         return (self.x, self.y)
 
     def getDesti(self):
         if self.path.length != 0:
-            return self.path[-1]    # 최종 목표 노드 (self.next_node와 동일)
+            return self.path[-1]    # 최종 목표 노드 (self.desti_node와 동일)
         else:                       # 도착지가 없는 경우
-            return 0
+            return -1
 
     def getBattery(self):
         return self.battery
@@ -125,19 +123,23 @@ class Vehicle:
     def threadFunc(self):
         while True:
             # 충돌여부 조사 (다른 차량 정보 모두 필요) -> 모든 차량 정보일텐데 본인은 어떻게 제외시킬까?
-            for i in range(len(cars)):
-                if self is cars[i]: # 이게 될까?
+            for i in range(len(CARS_LIST)):
+                if self is CARS_LIST[i]: # 이게 될까?
                     continue
-                crashed = self.checkCrash(cars[i])
+                crashed = self.checkCrash(CARS_LIST[i])
                 if crashed:
                     self.status = 91
                     return -1   # 종료..?
             # 현재 상태를 파악
-            # 대기
-            if self.status == 00:
+            # 초기상태 / 대기
+            if self.status == 00 or self.status == 10:
                 # Core에서 명령 있는지 확인->명령생기면 self.status바뀌므로 여기서 처리ㄴㄴ
                 # 대기 카운트 += 1초
-                # 대기 카운트 >= 5: Core에 알림! 복귀, 충전 명령은 Core에서 해주는걸로?
+                self.count += 1
+                if self.count >= 5:
+                    self.count = 0
+                    # Core에 알림! 복귀, 충전 명령은 Core에서 해주는걸로?
+                    pass
                 # 대기 배터리 방전
                 self.battery -= self.DISCHARGE_WAIT/60/self.time   # 분->초->배속
             # 반송 중
@@ -146,25 +148,36 @@ class Vehicle:
                 if self.path.length == 0:
                     # LOAD/UNLOAD
                     if self.loaded:
+                        # 포트가 unload가능한 상태인지 확인
+                        pass
+                        self.status = 40
                         sleep(30*self.time)   # 그냥 30초 쉴지, count 방식으로 쉴지
                         self.unload()
+                        # 대기 상태로 전환
+                        self.status = 10
                     else:
+                        # 포트가 load가능한 상태인지 확인
+                        pass
+                        self.status = 30
                         sleep(30*self.time)
                         self.load()
+                        # 대기 상태로 전환
+                        self.status = 10
 
                 # 현재 경유지에 도착했다면
-                elif current_location == self.node:    # 현재 x, y위치가 어떤 노드인지 
-                    self.node = self.path.pop(0)
+                elif self.getPos() == NODE_LIST[self.node].getPos():
+                    self.node = self.path.pop(0)    # node 갱신, path에서 삭제
                 # 목적지가 있다면
                 # 회전 & 가감속
-                angle_diff = self.getAngle(self.getDesti(self.path[0])) - self.angle
+                angle_diff = self.getAngle(NODE_LIST[self.node]) - self.angle
                 if angle_diff==0:   # 현재 목적지를 향해 보고 있다
-                    distance = self.getDesti() - self.getPos() # 현재 목적지와의 거리 # 벡터
-                    # 도착했다는 것은 어떻게 할까? distance가 정확히 0이 될 일은 거의 없을텐데->일정 threshold 이하면 그 위치로 보정
-                    if distance <= 0.000001 and self.velocity <= 0.01:
-                        self.x = self.getDesti().x
-                        self.y = self.getDesti().y
-                        continue
+                    distance = NODE_LIST[self.node].getPos() - self.getPos() # 현재 목적지와의 거리 # (x, y)
+                    # 벡터->스칼라 변환 필요. 같은 방위각이므로 x,y 중 하나는 0일 것임.
+                    if isclose(distance[0], 0):
+                        distance = distance[1]
+                    else:
+                        distance = distance[0]
+                    pass
                     if distance <= self.getBrakeDis():  # 지금부터 브레이크를 밟아야 현재 목적지에서 정지
                         self.velocity -= self.ACCEL # velocity: m/min, ACCEL: m/min
                     else:
@@ -181,6 +194,11 @@ class Vehicle:
                     elif isclose(self.angle, 180):
                         self.y -= self.velocity/100*6/self.time # m/min->1000mm/60sec->배속
 
+                    # 도착했다는 것은 어떻게 할까? distance, x, y가 정확히 0이 될 일은 거의 없을텐데->일정 threshold 이하면 그 위치로 보정
+                    if distance <= 0.000001 and self.velocity <= 0.01:
+                        self.x = self.getDesti().x
+                        self.y = self.getDesti().y
+                        continue
 
                 else:   # 현재 목적지를 보고 있지 않다면, 회전을 해야겠지
                     # 각도 차이에 따라 더할지 뺄지 로직 필요
@@ -216,9 +234,7 @@ class Vehicle:
                     # self.angle += (self.ROTATE_SPEED)/self.time    # 초->배속
                     
                 # 동작 배터리 방전
-                self.battery += self.CHARGE_SPEED/60/self.time   # 분->초->배속
-                # 반송 완료 후 대기
-                self.status = 00
+                self.battery -= self.DISCHARGE_WORK/60/self.time   # 분->초->배속
 
             # 충전소로 복귀
             elif self.status == 22:
@@ -232,6 +248,7 @@ class Vehicle:
                 # 배터리 과충전 불가
                 if self.battery > 100:
                     self.battery = 100
+                # 충전 완료 됐다고 Core에 알리기
             # 에러
             elif self.status == 91 or self.status == 99:
                 pass
