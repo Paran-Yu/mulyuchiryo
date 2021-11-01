fb67024cf8329d25da95ab65f4968f76722ea978
h_nuls55 <h_nuls55@naver.com>
2021-11-01 15:03:53 +0900

    [Simulator] fix: map변수를 mapdata로 수정

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ node_list = []
 path_list = []
 vehicle_list = []
 
-img, map = mapreader.read_layout()
+img, mapdata = mapreader.read_layout()
 port_list, wait_list, node_list, path_list, vehicle_list = mapreader.read_component()
 
 # simulate 시작하면 simulator 함수 시작하도록
