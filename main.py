e3eef29e1bf7e3bfeeb21cf48af945b1c27b28fc
h_nuls55 <h_nuls55@naver.com>
2021-11-01 15:07:44 +0900

    [Simulate] feat: 배속 변수 추가

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import mapreader
 from simulator import simulator
 
+# simulate attribute
+simulate_speed = 1
+
+# layout component
 port_list = []
 wait_list = []
 node_list = []
@@ -11,4 +15,4 @@ img, mapdata = mapreader.read_layout()
 port_list, wait_list, node_list, path_list, vehicle_list = mapreader.read_component()
 
 # simulate 시작하면 simulator 함수 시작하도록
-simulator(port_list, wait_list, vehicle_list)
+simulator(simulate_speed, port_list, wait_list, vehicle_list)
