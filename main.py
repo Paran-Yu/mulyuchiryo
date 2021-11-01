094b733d1da4f50e6fd8b188a5feaae957b70611
h_nuls55 <h_nuls55@naver.com>
2021-11-01 16:33:24 +0900

    [Simulator] fix: VEHICLE_STATUS를 main으로 이동

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import mapreader
 
+# Declare Variables
 port_list = []
 wait_list = []
 node_list = []
 path_list = []
 vehicle_list = []
+
+VEHICLE_STATUS = {
+    00: "INIT",
+    10: "WAITING",
+    11: "WAITING&LOADED",
+    20: "MOVING TO WAIT",
+    21: "MOVING TO UNLOAD",
+    22: "MOVING TO LOAD",
+    23: "MOVING TO CHARGE",
+    30: "LOADING",
+    40: "UNLOADING",
+    80: "CHARGING",
+    81: "CHARGING&LOADED",
+    91: "COLLIDED",
+    99: "ERROR"
+}
 
 img, map = mapreader.read_layout()
 port_list, wait_list, node_list, path_list, vehicle_list = mapreader.read_component()
