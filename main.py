33dd61ceef08f430d1584987247d87b66b1e1880
h_nuls55 <h_nuls55@naver.com>
2021-11-01 15:03:13 +0900

    [Simulator] feat: main에 simulator 실행 코드 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mapreader
+from simulator import simulator
 
 port_list = []
 wait_list = []
@@ -8,3 +9,6 @@ vehicle_list = []
 
 img, map = mapreader.read_layout()
 port_list, wait_list, node_list, path_list, vehicle_list = mapreader.read_component()
+
+# simulate 시작하면 simulator 함수 시작하도록
+simulator(port_list, wait_list, vehicle_list)
