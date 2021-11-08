79d1c36502f02a564dd2c23345831df9dc4180ad
h_nuls55 <h_nuls55@naver.com>
2021-11-08 11:25:25 +0900

    [Simulator] fix: 반송물 생성 중복 제거

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -14,19 +14,10 @@ def port_init(port_list):
 
 def port_update(port_list):
     for x in port_list:
-        # LOAD: 반송물이 있어도 생산은 계속된다
-        if x.TYPE == "load":
+        # LOAD: 반송물이 사라져야 카운트 시작
+        # UNLOAD: 반송물을 받은 후에야 count가 reset 된다.
+        if x.status == 0:
             x.count += 1
             if x.count == x.FREQ:
                 x.status = 1
                 x.count = 0
-        # UNLOAD: 반송물을 받은 후에야 count가 reset 된다.
-        elif x.TYPE == "unload":
-            if x.status == 0:
-                x.count += 1
-                if x.count == x.FREQ:
-                    x.status = 1
-                    x.count = 0
-
-        # TODO
-        # 1. load/unload 동작 중에 주기가 다시 오는 경우 어떻게 처리?
