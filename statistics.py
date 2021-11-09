a50408128ce876171fcc8aab29f6cd7361619a25
h_nuls55 <h_nuls55@naver.com>
2021-11-09 23:00:43 +0900

    [Stat] feat: vehicle 충전률 plot 기능 추가

--- statistics.py
+++ statistics.py
@@ -6,13 +6,24 @@ db = DB()
 db.db_init()
 
 def vehicle_charge():
-    v1 = [100, 80, 60, 40, 20]
-    v2 = [50, 70, 90, 70, 50]
+    charge_list = db.get_vehicle_charge()
+    # v1 = [100, 80, 60, 40, 20]
+    # v2 = [50, 70, 90, 70, 50]
 
     dt = 1
-    t = np.arrange(0, len(v1), dt)
+    t = np.arange(0, len(charge_list[0]['battery']), dt)
+
+    # TODO
+    # 시간 간격 dt 조정 기능 - UI와 협의
 
     fig, ax = plt.subplots()
-    ax.plot(t, v1, t, v2)
+    for x in charge_list:
+        ax.plot(t, charge_list[0]['battery'], label=charge_list[0]['name'])
 
-vehicle_charge()
+    ax.set_xlabel('Time[sec]')
+    ax.set_ylabel('Battery[%]')
+    ax.grid(True)
+    plt.legend()
+    plt.show()
+
+# vehicle_charge()
