5fc77914b44b1b5413f75dbacfdaf7559bd008b2
h_nuls55 <h_nuls55@naver.com>
2021-11-09 15:50:51 +0900

    [Stat] feat: db 객체화

--- db.py
+++ db.py
@@ -1,59 +1,62 @@
 import sqlite3
 
-conn = sqlite3.connect("simul_data.db")
 
+class DB:
+    def __init__(self):
+        try:
+            self.db = sqlite3.connect("simul_data.db")
+        except:
+            print("DB Connection failed")
+            exit(1)
 
-def db_init():
-    cur = conn.cursor()
+    def db_init(self):
+        cur = self.db.cursor()
 
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='scene'")
-    returns = cur.fetchall()
-    if len(returns) == 0:
-        conn.execute('CREATE TABLE scene(\
-        id INTEGER PRIMARY KEY AUTOINCREMENT)')
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='scene'")
+        returns = cur.fetchall()
+        if len(returns) == 0:
+            cur.execute('CREATE TABLE scene(\
+            id INTEGER PRIMARY KEY AUTOINCREMENT)')
 
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='vehicle'")
-    returns = cur.fetchall()
-    if len(returns) == 0:
-        conn.execute('CREATE TABLE vehicle(\
-        id INTEGER PRIMARY KEY AUTOINCREMENT,\
-        scene_id INTEGER,\
-        time INTEGER,\
-        name TEXT,\
-        start_node INTEGER,\
-        desti_node INTEGER,\
-        cur_node INTEGER,\
-        x REAL,\
-        y REAL,\
-        status INTEGER,\
-        velocity REAL,\
-        angle REAL,\
-        battery REAL,\
-        loaded INTEGER,\
-        FOREIGN KEY(scene_id) REFERENCES scene(id)\
-        )')
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='vehicle'")
+        returns = cur.fetchall()
+        if len(returns) == 0:
+            cur.execute('CREATE TABLE vehicle(\
+            id INTEGER PRIMARY KEY AUTOINCREMENT,\
+            scene_id INTEGER,\
+            time INTEGER,\
+            name TEXT,\
+            start_node INTEGER,\
+            desti_node INTEGER,\
+            cur_node INTEGER,\
+            x REAL,\
+            y REAL,\
+            status INTEGER,\
+            velocity REAL,\
+            angle REAL,\
+            battery REAL,\
+            loaded INTEGER,\
+            FOREIGN KEY(scene_id) REFERENCES scene(id)\
+            )')
 
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='command'")
-    returns = cur.fetchall()
-    if len(returns) == 0:
-        conn.execute('CREATE TABLE command(\
-        id INTEGER PRIMARY KEY AUTOINCREMENT,\
-        scene_id INTEGER,\
-        vehicle_id INTEGER,\
-        start_node INTEGER,\
-        desti_node INTEGER,\
-        path TEXT,\
-        type INTEGER,\
-        created_at TEXT,\
-        is_checked INTEGER,\
-        FOREIGN KEY(scene_id) REFERENCES scene(id)\
-        FOREIGN KEY(vehicle_id) REFERENCES vehicle(id)\
-        )')
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='command'")
+        returns = cur.fetchall()
+        if len(returns) == 0:
+            cur.execute('CREATE TABLE command(\
+            id INTEGER PRIMARY KEY AUTOINCREMENT,\
+            scene_id INTEGER,\
+            vehicle_id INTEGER,\
+            start_node INTEGER,\
+            desti_node INTEGER,\
+            path TEXT,\
+            type INTEGER,\
+            created_at TEXT,\
+            is_checked INTEGER,\
+            FOREIGN KEY(scene_id) REFERENCES scene(id)\
+            FOREIGN KEY(vehicle_id) REFERENCES vehicle(id)\
+            )')
 
-    conn.commit()
-
-
-conn.close()
+        cur.commit()
 
 
 
