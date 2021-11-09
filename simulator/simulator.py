7884ede8e71d898da1c9bbdb92a261a80bb8d191
h_nuls55 <h_nuls55@naver.com>
2021-11-10 00:21:26 +0900

    [Simulator] feat: 루틴 수행 부분 추가

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -38,24 +38,6 @@ def wait_init(wait_list, vehicle_list):
 # VEHICLE
 def vehicle_update(node_list, vehicle_list):
     for vehicle in vehicle_list:
-        # 1. 충돌 체크
-        # n^2이 되는데 반드시 필요한지 검토 필요
+        vehicle.vehicle_routine(node_list)
+        # TODO: DB에 기록
 
-        # 2. 명령 수행
-        # status와 현재 받은 cmd를 분리
-        # load
-        if vehicle.cmd == 22:
-            pass
-        # unload
-        elif vehicle.cmd == 21:
-            pass
-        # wait
-        elif vehicle.cmd == 20:
-            pass
-        # charge
-        elif vehicle.cmd == 23:
-            pass
-
-
-def move_vehicle(vehicle):
-    pass
