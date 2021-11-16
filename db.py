68fd6c9864fd64f57cec1efcdbc7b5ed09467d7a
h_nuls55 <h_nuls55@naver.com>
2021-11-16 15:27:01 +0900

    [Stat] feat: scene 추가 기능

--- db.py
+++ db.py
@@ -4,13 +4,17 @@ import sqlite3
 class DB:
     def __init__(self):
         try:
-            self.db = sqlite3.connect("simul_data.db")
+            self.conn = sqlite3.connect("simul_data.db")
         except:
             print("DB Connection failed")
             exit(1)
 
+        self.scene_num = -1
+
+        self.db_init()
+
     def db_init(self):
-        cur = self.db.cursor()
+        cur = self.conn.cursor()
 
         cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='scene'")
         returns = cur.fetchall()
@@ -56,12 +60,36 @@ class DB:
             FOREIGN KEY(vehicle_id) REFERENCES vehicle(id)\
             )')
 
-
     def db_clear(self):
-        cur = self.db.cursor()
+        cur = self.conn.cursor()
         cur.execute("DROP table scene")
         cur.execute("DROP table vehicle")
         cur.execute("DROP table command")
+
+    def put_scene_num(self, num):
+        cur = self.conn.cursor()
+        cur.execute(f'INSERT INTO scene VALUES ({num})')
+        print("new scene: ", num)
+        self.conn.commit()
+
+    def put_command(self):
+        pass
+
+    def put_vehicle_status(self):
+        pass
+
+    def set_scene_num(self, num):
+        self.scene_num = num
+
+    def get_scene_num(self):
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM scene ORDER BY ROWID DESC LIMIT 1")
+        result = cur.fetchall()
+        print(result)
+        if len(result) == 0:
+            return -1
+        else:
+            return result[0]
 
     def get_vehicle_work(self):
         pass
@@ -75,3 +103,15 @@ class DB:
     def get_node_freq(self):
         pass
 
+    def create_new_scene(self):
+        last_num = self.get_scene_num()
+        print(last_num)
+        if last_num == -1:
+            self.put_scene_num(1)
+            self.set_scene_num(1)
+        else:
+            self.put_scene_num(last_num[0] + 1)
+            self.set_scene_num(last_num[0] + 1)
+
+#db = DB()
+#db.create_new_scene()
